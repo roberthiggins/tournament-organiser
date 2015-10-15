52ce289c6d4dae8ad4352c28111986239f98f2bd
Robert Higgins <robert.h.higgins@gmail.com>
2015-10-15 14:38:02 +1300

    [Webserver] Added some error checking to the register page

--- webserver/src/static/js/add-player.js
+++ webserver/src/static/js/add-player.js
@@ -1,7 +1,31 @@
 $(function() {
+
+    function displayError(msg) {
+        $('div.errors ul').append('<li>' + msg + '</li>');
+        $('div.errors').show();
+    };
     
     $(document).ready( function addSubmitListener(){
         $('#btnRegister').click(function() {
+
+            var errors = 0;
+
+            $('div.errors ul').empty();
+
+            if ( $.trim($('input#inputPassword').val()) == '' ) {
+                displayError("Please enter a password");
+                errors++;
+            }
+
+            if ( $('input#inputPassword').val() !== $('input#inputConfirmPassword').val() ) {
+                displayError("Your passwords don't match");
+                errors++;
+            }
+
+            if (errors > 0 ) {
+                return;
+            }
+
             $.ajax({
                 url: '/addPlayer',
                 data: $('form').serialize(),
