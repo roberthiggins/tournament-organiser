d2d5820159a84ed987b93db0a710faf6d280abad
Robert Higgins <robert.h.higgins@gmail.com>
2015-11-19 14:13:31 +1300

    [Refactor] Adding some more test data

--- database/setup/999_test_data.sql
+++ database/setup/999_test_data.sql
@@ -8,8 +8,13 @@ DECLARE lastid int := 0;
 BEGIN
     INSERT INTO account VALUES (DEFAULT, 'foo@bar.com') RETURNING id INTO lastid;
     INSERT INTO player VALUES (lastid, 'stevemcqueen', NULL);
+    INSERT INTO account_security VALUES (lastid, '$5$rounds=535000$gEkrAmEJxdn30HMR$HPPOeXufYDksVGLSUbj5TqJVKKRTBsU31VsetE9oeI0');
     INSERT INTO account VALUES (DEFAULT, 'foo@bar.com') RETURNING id INTO lastid;
     INSERT INTO player VALUES (lastid, 'rick_james', NULL);
+    INSERT INTO account_security VALUES (lastid, '$5$rounds=535000$gEkrAmEJxdn30HMR$HPPOeXufYDksVGLSUbj5TqJVKKRTBsU31VsetE9oeI0');
+    INSERT INTO account VALUES (DEFAULT, 'chalie_murphy@darkness.com') RETURNING id INTO lastid;
+    INSERT INTO player VALUES (lastid, 'charlie_murphy', NULL);
+    INSERT INTO account_security VALUES (lastid, '$5$rounds=535000$1ChlmvAIh/6yDqVg$wn8vZxK1igRA17V8pjMr90ph3Titr35DF5X5DYSLpv.');
 END $$;
 
 INSERT INTO registration VALUES('stevemcqueen', 'painting_test');
