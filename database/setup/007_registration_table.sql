56283eb2b0a5c45d2fb7fa28c495022eed9fd56d
Robert Higgins <robert.h.higgins@gmail.com>
2015-11-20 16:56:22 +1300

    [Database] Test data passing

--- database/setup/007_registration_table.sql
+++ database/setup/007_registration_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE registration(
     player_id           VARCHAR REFERENCES player(username),
     tournament_id       VARCHAR REFERENCES tournament(name),
-    has_paid		boolean not null,
-    turned_up		boolean not null,
-    list_accept		boolean not null,
+    has_paid            boolean not null default False,
+    turned_up           boolean not null default False,
+    list_accept         boolean not null default False,
     PRIMARY KEY (player_id, tournament_id)
 );
