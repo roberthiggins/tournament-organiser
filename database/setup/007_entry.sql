3d7cae0161ae96470bd31bbf2a8677284e6cd086
Robert Higgins <robert.h.higgins@gmail.com>
2015-11-20 14:54:39 +1300

    [Database] Pass to data

--- database/setup/007_entry.sql
+++
@@ -1,6 +0,0 @@
-create table entry (
-	id 		serial not null unique,
-	tournament_id   varchar unique references tournament(name),
-	is_accepted	bool not null 
-);
--- Why do we need is_accepted? The registration gets finalised. An entry assumes accepted I would suppose.
--- database/setup/007_feedback_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE feedback(
-    id          SERIAL PRIMARY KEY,
-    feedback    VARCHAR NOT NULL UNIQUE,
-    time        TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
-);
