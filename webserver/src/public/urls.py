187ae9e7b4adc49371404cd70831dad5b37ff688
Robert Higgins <robert.h.higgins@gmail.com>
2015-11-24 10:00:47 +1300

    [Webserver] Splitting the views into public and private

--- webserver/src/public/urls.py
+++ webserver/src/public/urls.py
@@ -1,26 +1,29 @@
 # pylint: disable=C0111
 from django.conf.urls import patterns, url
 
-from public import views
+from public import public_views, views
 
 # pylint: disable=C0103
 urlpatterns = patterns('',
 
-    # Basic pages. These are part of a regular journey
-    url(r'^$', views.index, name='index'),
+    # Public views
+    url(r'^$', public_views.index, name='index'),
+    url(r'^login$', public_views.login, name='login'),
+    url(r'^signup$', public_views.create_account, name='create_account'),
+    url(r'^tournament/(?P<tournament_id>.+)$',
+        public_views.tournament, name='tournament'),
+    url(r'^tournament/$',
+        public_views.list_tournaments, name='apply_for_tournament'),
+    url(r'^tournaments$',
+        public_views.list_tournaments, name='list_tournaments'),
+
+    # Logged in views
     url(r'^createtournament$',
         views.create_tournament, name='create_tournament'),
     url(r'^feedback$', views.feedback, name='feedback'),
-    url(r'^login$', views.login, name='login'),
     url(r'^logout$', views.logout, name='logout'),
     url(r'^registerforatournament$',
         views.register_for_tournament, name='apply_for_tournament'),
-    url(r'^signup$', views.create_account, name='create_account'),
     url(r'^suggestimprovement$',
         views.suggest_improvement, name='suggest_improvement'),
-    url(r'^tournament/(?P<tournament_id>.+)$',
-        views.tournament, name='tournament'),
-    url(r'^tournament/$',
-        views.register_for_tournament, name='apply_for_tournament'),
-    url(r'^tournaments$', views.list_tournaments, name='list_tournaments'),
 )
--- webserver/src/public/views.py
+++ webserver/src/public/views.py
@@ -4,42 +4,12 @@ Basic URL mappings for the webserver
 
 import json
 
-from django.contrib import auth
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from forms import AddTournamentForm, ApplyForTournamentForm, \
-CreateAccountForm, FeedbackForm, LoginForm
+from forms import AddTournamentForm, ApplyForTournamentForm, FeedbackForm
 from public.view_helpers import from_dao
-
-def index(request):                                     # pylint: disable=W0613
-    """The index"""
-    return render_to_response('index.html')
-
-def create_account(request):
-    """Page to create a new account"""
-
-    form = CreateAccountForm()
-
-    if request.method == 'POST':
-        form = CreateAccountForm(request.POST)
-        if form.is_valid():                             # pylint: disable=E1101
-            form.save()
-
-        response = from_dao('/addPlayer', form)
-
-        if  response.status_code == 200:
-            return HttpResponse(response)
-        else:
-            form.add_error(None, response.content)      # pylint: disable=E1103
-
-    return render_to_response(
-        'create-a-player.html',
-        {'form': form},
-        RequestContext(request)
-    )
 
 @login_required
 def create_tournament(request):
@@ -90,76 +60,6 @@ def feedback(request):
         context_dict,
         RequestContext(request)
     )
-
-def render_login(request, form):
-    """ Render the login page from the template """
-    return render_to_response(
-        'login.html',
-        {'form': form},
-        RequestContext(request)
-    )
-
-def create_or_update_user(login_creds):
-    """
-    Create a new local user or update from db.
-
-    A failure to authenticate locally could be because the user details have
-    been updated on the db but not on this webserver
-
-    TODO security to ensure the user is actually in the db
-    """
-    u_name = login_creds.cleaned_data['inputUsername']
-    p_word = login_creds.cleaned_data['inputPassword']
-    user = json.load(from_dao('/userDetails/%s' % u_name)).get(u_name)
-
-    try:
-        local_user = User.objects.get(username=u_name)
-        local_user.set_password(p_word)
-        local_user.email = user[0]
-        local_user.save()
-    except User.DoesNotExist:                           # pylint: disable=E1101
-        User.objects.create_user(
-            username=u_name,
-            email=user[0],
-            password=p_word)
-
-def list_tournaments(request):
-    """ Get a list of tournaments"""
-    t_list = json.load(from_dao('/listtournaments'))['tournaments']
-    return render_to_response(
-        'tournament-list.html',
-        {'tournaments': t_list},
-        RequestContext(request)
-    )
-
-def login(request):
-    """Login page"""
-
-    login_creds = LoginForm()
-    if request.user.is_authenticated():
-        return render_to_response('You are already logged in')
-
-    if request.method == 'POST':
-        login_creds = LoginForm(request.POST)
-
-        username = request.POST.get('inputUsername', '')
-        password = request.POST.get('inputPassword', '')
-        if username == '' or password == '':
-            return render_login(request, login_creds)
-
-        response = from_dao('/login', login_creds)
-        if  response.status_code == 200:
-            # The user might exist in the db but not on this webserver.
-            create_or_update_user(login_creds)
-            user = auth.authenticate(username=username, password=password)
-            auth.login(request, user)
-        else:
-            login_creds.add_error(None, 'Username or password incorrect') # pylint: disable=E1103
-            return render_login(request, login_creds)
-
-        return HttpResponseRedirect(request.REQUEST.get('next', '/'))
-
-    return render_login(request, login_creds)
 
 def logout(request):
     """ logout the user from current request """
@@ -216,19 +116,3 @@ def suggest_improvement(request):
         context_dict,
         RequestContext(request)
     )
-
-def tournament(request, tournament_id):
-    """ See information about a single tournament"""
-    if tournament_id is None or request.method == 'POST':
-        return register_for_tournament(request)
-
-    response = from_dao('/tournamentDetails/%s' % tournament_id)
-    try:
-        t_info = json.load(response)
-        return render_to_response(
-            'tournament-info.html',
-            {'id': tournament_id, 'info': t_info},
-            RequestContext(request)
-        )
-    except AttributeError:
-        return HttpResponse(response)
