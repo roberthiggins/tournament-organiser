264dcdd772365358056167d2fa7e779d61af0159
Robert Higgins <robert.h.higgins@gmail.com>
2015-12-08 16:37:12 +1300

    [Database] Removing unique constraint
    It would constrain mulitple tournaments. Instead we place the
    heavy burdern of competence on tournament organisers.

--- database/setup/011_score_category.sql
+++ database/setup/011_score_category.sql
@@ -7,7 +7,7 @@ tournament points; A painting score might be ignored for determing matchups; etc
 CREATE TABLE score_category(
     id                  SERIAL PRIMARY KEY,
     tournament_id       VARCHAR NOT NULL REFERENCES tournament(name),
-    display_name        VARCHAR UNIQUE NOT NULL,
+    display_name        VARCHAR NOT NULL,
     percentage          INTEGER NOT NULL DEFAULT 100
 );
 COMMENT ON COLUMN score_category.percentage IS 'The raw score should be multiplied by this.';
