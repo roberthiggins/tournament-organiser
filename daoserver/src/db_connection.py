afb32fccd30b3a15b3419715333c6397ae812f28
Robert Higgins <robert.h.higgins@gmail.com>
2015-12-15 14:32:39 +1300

    [Refactor] [Daoserver] Tournament_db now uses a decorator for establishing connections

--- daoserver/src/db_connection.py
+++ daoserver/src/db_connection.py
@@ -2,6 +2,8 @@
 Class to make a connection to the db
 """
 
+from functools import wraps
+
 import os
 import psycopg2
 
@@ -32,3 +34,43 @@ class DBConnection(object):
     def __del__(self):
         if self.con:
             self.con.close()
+
+def db_conn(commit=False):
+    """A decorator that gives the function a db_conn to use (cur)"""
+    def decorator(func):                            # pylint: disable=C0111
+        @wraps(func)
+        def wrapped(*args, **kwargs):               # pylint: disable=C0111
+
+            glob = func.func_globals
+            sentinel = object()
+            old_values = {}
+
+            old_values['conn'] = glob.get('conn', sentinel)
+            old_values['cur'] = glob.get('cur', sentinel)
+
+            conn = DBConnection()
+            glob['conn'] = conn.con
+            glob['cur'] = glob['conn'].cursor()
+
+            try:
+                res = func(*args, **kwargs)
+                if commit:
+                    glob['conn'].commit()
+            except psycopg2.DatabaseError as err:
+                try:
+                    glob['conn'].rollback()
+                except AttributeError:
+                    pass
+                raise ValueError(err)
+            finally:
+                glob['cur'].close()
+                glob['conn'].close()
+                for var in ['cur', 'conn']:
+                    if old_values[var] is sentinel:
+                        del glob[var]
+                    else:
+                        glob[var] = old_values[var]
+
+            return res
+        return wrapped
+    return decorator
--- daoserver/src/tournament_db.py
+++ daoserver/src/tournament_db.py
@@ -4,68 +4,36 @@ This file contains code to connect to the tournament_db
 
 import psycopg2
 
-from db_connection import DBConnection
+from db_connection import db_conn
 
+# pylint: disable=E0602,R0201
 class TournamentDBConnection(object):
     """
     Connection class to the tournament database
     """
-    def __init__(self):
-        self.db_conn = DBConnection()
-        self.con = self.db_conn.con
-
-    def enter_game_score(self, tournament, round_num, scores):
-        """
-        Enters a score for a game into tournament for all entries involved
-
-        Expects: All fields required
-
-        Assumption: That the entries in scores exist
-        """
-        if not tournament or not round_num or len(scores) < 2:
-            raise RuntimeError('Enter the required fields')
-
-        if not self.tournament_exists(tournament):
-            raise RuntimeError('Unknown tournament: ' + tournament)
-
-        raise NotImplementedError("enter game score not implemented")
-
+    @db_conn()
     def get_mission(self, tournament, round_id):
         """Get mission for given round"""
-        try:
-            cur = self.con.cursor()
-            cur.execute("SELECT mission FROM tournament_round \
-                        WHERE tournament_name = %s AND ordering = %s",
-                        [tournament, round_id])
-            existing = cur.fetchone()
-            return existing[0]
-        except psycopg2.DatabaseError as err:
-            raise ValueError(err)
+        cur.execute("SELECT mission FROM tournament_round \
+                    WHERE tournament_name = %s AND ordering = %s",
+                    [tournament, round_id])
+        return cur.fetchone()[0]
 
+    @db_conn(commit=True)
     def set_mission(self, tournament, round_id, mission):
         """Set mission for given round"""
-        try:
-            cur = self.con.cursor()
-            cur.execute("UPDATE tournament_round SET mission = %s \
-                        WHERE tournament_name = %s AND ordering = %s",
-                        [mission, tournament, round_id])
-            self.con.commit()
-        except psycopg2.DatabaseError as err:
-            self.con.rollback()
-            raise ValueError(err)
+        cur.execute("UPDATE tournament_round SET mission = %s \
+                    WHERE tournament_name = %s AND ordering = %s",
+                    [mission, tournament, round_id])
 
+    @db_conn()
     def tournament_exists(self, name):
         """Check if a tournament exists with the passed name"""
-        try:
-            cur = self.con.cursor()
-            cur.execute("SELECT COUNT(*) FROM tournament WHERE name = %s",
-                        [name])
-            existing = cur.fetchone()
-            return existing[0] > 0
-        except psycopg2.DatabaseError as err:
-            self.con.rollback()
-            raise err
+        cur.execute("SELECT COUNT(*) FROM tournament WHERE name = %s",
+                    [name])
+        return cur.fetchone()[0] > 0
 
+    @db_conn(commit=True)
     def add_tournament(self, tournament):
         """
         Add a tournament.
@@ -74,18 +42,11 @@ class TournamentDBConnection(object):
                             'name' - unique name,
                             'date' - YY-MM-DD}
         """
-        try:
-            cur = self.con.cursor()
-            cur.execute(
-                "INSERT INTO tournament VALUES (default, %s, %s)",
-                [tournament['name'], tournament['date']])
-            self.con.commit()
+        cur.execute(
+            "INSERT INTO tournament VALUES (default, %s, %s)",
+            [tournament['name'], tournament['date']])
 
-        except psycopg2.DatabaseError as err:
-            self.con.rollback()
-            print 'Database Error %s' % err
-            raise err
-
+    @db_conn(commit=True)
     def create_score_category(self, category, tournament_id, percentage):
         """
         Create a score category for a tournament.
@@ -101,62 +62,46 @@ class TournamentDBConnection(object):
         except ValueError:
             raise ValueError('percentage must be an integer')
 
-        try:
-            cur = self.con.cursor()
+        cur.execute(
+            "SELECT SUM(percentage) FROM score_category \
+            WHERE tournament_id = %s", [tournament_id])
+        existing = cur.fetchone()
+        existing_total = existing[0] if existing[0] is not None else 0
+        if (existing_total + percentage) > 100:
+            raise ValueError('percentage too high: {}'.format(category))
 
-            cur.execute(
-                "SELECT SUM(percentage) FROM score_category \
-                WHERE tournament_id = %s", [tournament_id])
-            existing = cur.fetchone()
-            existing_total = existing[0] if existing[0] is not None else 0
-            if (existing_total + percentage) > 100:
-                raise ValueError('percentage too high: {}'.format(category))
+        cur.execute(
+            "INSERT INTO score_category VALUES(DEFAULT, %s, %s, %s)",
+            [tournament_id, category, percentage]
+        )
 
-            cur.execute(
-                "INSERT INTO score_category VALUES(DEFAULT, %s, %s, %s)",
-                [tournament_id, category, percentage]
-            )
-            self.con.commit()
-
-        except psycopg2.DatabaseError as err:
-            self.con.rollback()
-            print 'Database Error %s' % err
-            raise err
-
+    @db_conn()
     def list_score_categories(self, tournament_id):
         """
         Get score_categories associated with a tournament.
         e.g. [{ 'name': 'painting', 'percentage': 20 }]
         """
-        try:
-            cur = self.con.cursor()
-            cur.execute(
-                "SELECT id, display_name, percentage FROM score_category \
-                WHERE tournament_id = %s", [tournament_id])
-            raw_list = cur.fetchall()
-            return [{'id': x[0],
-                    'name': x[1],
-                    'percentage': x[2]} for x in raw_list]
-        except psycopg2.DatabaseError as err:
-            print 'Database Error %s' % err
-            raise err
+        cur.execute(
+            "SELECT id, display_name, percentage FROM score_category \
+            WHERE tournament_id = %s", [tournament_id])
+        raw_list = cur.fetchall()
+        return [{'id': x[0],
+                'name': x[1],
+                'percentage': x[2]} for x in raw_list]
 
+    @db_conn()
     def list_tournaments(self):
         """Get a list of tournaments"""
-        try:
-            cur = self.con.cursor()
-            cur.execute(
-                "SELECT name, date, num_rounds, score_id FROM tournament")
-            raw_list = cur.fetchall()
+        cur.execute(
+            "SELECT name, date, num_rounds, score_id FROM tournament")
+        raw_list = cur.fetchall()
 
-            return [{'name': x[0],
-                    'date': x[1],
-                    'rounds': x[2],
-                    'scoring': x[3]} for x in raw_list]
-        except psycopg2.DatabaseError as err:
-            print 'Database Error %s' % err
-            raise err
+        return [{'name': x[0],
+                'date': x[1],
+                'rounds': x[2],
+                'scoring': x[3]} for x in raw_list]
 
+    @db_conn()
     def tournament_details(self, name):
         """
         Get information about a tournament.
@@ -165,16 +110,11 @@ class TournamentDBConnection(object):
         if not self.tournament_exists(name):
             raise RuntimeError('No information is available on "%s" ' % name)
 
-        try:
-            cur = self.con.cursor()
-            cur.execute("SELECT * FROM tournament WHERE name = %s",
-                [name])
-            return cur.fetchone()
+        cur.execute("SELECT * FROM tournament WHERE name = %s",
+            [name])
+        return cur.fetchone()
 
-        except psycopg2.DatabaseError as err:
-            print 'Database Error %s' % err
-            raise err
-
+    @db_conn(commit=True)
     def set_score_key(self, key, category, min_val, max_val):
         """
         Create a score that entries can get in the tournament. This should be
@@ -202,20 +142,14 @@ class TournamentDBConnection(object):
             raise ValueError('Maximum Score must be an integer')
 
         try:
-            cur = self.con.cursor()
             cur.execute(
                 "INSERT INTO score_key VALUES(default, %s, %s, %s, %s)",
                 [key, max_val, min_val, category])
-            self.con.commit()
 
         except psycopg2.IntegrityError:
-            self.con.rollback()
-            raise RuntimeError('Score already set')
-        except psycopg2.DatabaseError as err:
-            self.con.rollback()
-            print 'Database Error %s' % err
-            raise RuntimeError(err)
+            raise psycopg2.DatabaseError('Score already set')
 
+    @db_conn()
     def get_score_keys_for_round(self, tournament_id, round_id):
         """
         Get all the score keys, and the information from their rows, for a
@@ -226,24 +160,16 @@ class TournamentDBConnection(object):
         except ValueError:
             raise ValueError('Round ID must be an integer')
 
-        try:
-            cur = self.con.cursor()
-            cur.execute(
-                "SELECT COUNT(*) FROM tournament_round \
-                WHERE tournament_name = %s AND id = %s",
-                [tournament_id, round_id]
-            )
-            if cur.fetchone()[0] == 0:
-                raise ValueError("Round {} doesn't exist in {}".format(
-                    round_id, tournament_id))
-            cur.execute(
-                "SELECT * FROM score_key k \
-                INNER JOIN round_score s ON s.score_key_id = k.id \
-                WHERE s.round_id = %s", [round_id])
-            return cur.fetchall()
-
-        except psycopg2.DatabaseError as err:
-            self.con.rollback()
-            print 'Database Error %s' % err
-            raise RuntimeError(err)
-
+        cur.execute(
+            "SELECT COUNT(*) FROM tournament_round \
+            WHERE tournament_name = %s AND id = %s",
+            [tournament_id, round_id]
+        )
+        if cur.fetchone()[0] == 0:
+            raise ValueError("Round {} doesn't exist in {}".format(
+                round_id, tournament_id))
+        cur.execute(
+            "SELECT * FROM score_key k \
+            INNER JOIN round_score s ON s.score_key_id = k.id \
+            WHERE s.round_id = %s", [round_id])
+        return cur.fetchall()
