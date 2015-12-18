164f24b0060d7d8d9f6becdff0ef70f67894c2da
Robert Higgins <robert.h.higgins@gmail.com>
2015-12-18 15:02:09 +1300

    [Daoserver] Shell class for a default table draw strategy

---
+++ daoserver/src/table_strategy.py
@@ -0,0 +1,37 @@
+"""
+Module to contain table allocation strategy
+"""
+
+class ProtestAvoidanceStrategy(object):
+    """
+    Allocate tables by determining all possible options and allocating based on
+    fewest protests.
+
+    Algorithm:
+        Protest:
+            - Assume a game has two entrants (who have a history of tables
+            played on)
+            - When a table is proposed one, or both, entrants may protest
+            - This gives a protest score of 0, 1, 2 (no-protest -> both protest)
+        Layout:
+            - A layout is a single possible configuration of entrants on
+            tables. Essentially this is one candidate for final configuration.
+        Allocation:
+            - For all possible layouts, determine the aggregate protest
+            - The least protested layout is chosen
+        Variations:
+            - There are four obvious variations:
+                - Avoid double protests - Least Outrage
+                - Avoid single protests - split happy and sad (perhaps good for
+                the competitive tables where playing there before-hand is a
+                real advantage)
+                - Avoid no protests - least variety
+                - Lowest aggregate protest - Utility happiness
+        Time-complexity:
+            - n3 for n games
+        Memory Complexity:
+            - 4 * n3 integers for n games.
+        Potential Trade-off:
+            A reasonably complex select to get table history for each player
+            could be stored with the entry.
+    """
