24b7a5e2bbf05c6dc8c2201d3c65074d45228838
Robert Higgins <robert.h.higgins@gmail.com>
2016-02-01 15:45:06 +1300

    [Db] Removing scores from game table

--- database/setup/009_game.sql
+++ database/setup/009_game.sql
@@ -1,10 +1,11 @@
 create table game(
-	id              serial primary key unique,
-	entry_1         integer references entry(id),
-	entry_2         integer references entry(id),
-	round_num       integer,
-	tourn           varchar references tournament(name),
-	entry_1_score   json,
-	entry_2_score   json,
-	table_num       integer
+    id                  serial unique,
+    entry_1             integer references entry(id) NOT NULL,
+    entry_2             integer references entry(id),
+    round_num           integer NOT NULL,
+    tourn               varchar NOT NULL references tournament(name),
+    table_num           integer,
+    protected_object_id integer references protected_object(id),
+    PRIMARY KEY(entry_1, round_num, tourn)
 );
+COMMENT ON TABLE game IS 'entry_2 is not needed in primary key as it may be null and the game should be unique with a single entry; An entry can only play in a single game.';
