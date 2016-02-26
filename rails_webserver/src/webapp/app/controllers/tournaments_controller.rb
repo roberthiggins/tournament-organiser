d617f8c4e7e0f0aba829e72cc500ba8da3597578
Robert Higgins <robert.h.higgins@gmail.com>
2016-02-26 13:14:48 +1300

    [Rails] create and edit  methods for tournament

--- rails_webserver/src/webapp/app/controllers/tournaments_controller.rb
+++ rails_webserver/src/webapp/app/controllers/tournaments_controller.rb
@@ -13,4 +13,28 @@ class TournamentsController < ApplicationController
     def show
         @tournament = Tournament.find(params[:id])
     end
+
+    def new
+        @tournament = Tournament.new
+    end
+
+    def create
+        @tournament = Tournament.new(tournament_params)
+
+        response = from_dao('addTournament', {
+            :inputTournamentName => @tournament.name,
+            :inputTournamentDate => @tournament.date
+        })
+
+        if response.code.to_i == 200
+            render 'show'
+        else
+            render 'new'
+        end
+    end
+
+    private
+        def tournament_params
+            params.require(:tournament).permit(:name, :date, :rounds)
+        end
 end
