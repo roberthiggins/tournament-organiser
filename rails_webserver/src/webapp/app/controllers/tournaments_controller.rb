c7a68a68b3f39899e43d64f1f42887e1f0c4e243
Robert Higgins <robert.h.higgins@gmail.com>
2016-02-26 13:13:24 +1300

    [Rails] show method for tournament

--- rails_webserver/src/webapp/app/controllers/tournaments_controller.rb
+++ rails_webserver/src/webapp/app/controllers/tournaments_controller.rb
@@ -5,7 +5,12 @@ class TournamentsController < ApplicationController
     include DaoConnection
 
     def index
-        @tournaments = JSON.parse(dao_get('listtournaments').body)["tournaments"]
+        @tournaments = JSON.parse(
+            from_dao('listtournaments').body)["tournaments"]
         @tournaments.map! { |tournament| Tournament.new(tournament) }
+    end
+
+    def show
+        @tournament = Tournament.find(params[:id])
     end
 end
