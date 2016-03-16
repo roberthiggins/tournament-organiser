fd2ec36004cb47af2ea4e896fdbd6a85f5e871e9
Robert Higgins <robert.h.higgins@gmail.com>
2016-03-16 16:36:53 +1300

    [Daoserver] Getting gameEntrants from ORM

--- daoserver/src/game.py
+++ daoserver/src/game.py
@@ -7,44 +7,41 @@ A game is simply a match between two entries. It is played on a table.
 
 from sqlalchemy.sql.expression import and_
 
-from db_connections.db_connection import db_conn
-from entry import Entry
+from models.game_entry import GameEntrant
 from models.score import RoundScore, ScoreCategory, ScoreKey, Score, db
+from models.tournament_entry import TournamentEntry
 from models.tournament_game import TournamentGame
 
-@db_conn()
 # pylint: disable=E0602
 def get_game_from_score(entry_id, score_key):
     """Given an entry and score_key, you should be able to work out the game"""
-    cur.execute(
-        "SELECT tournament, for_round, entry_id \
-        FROM player_score WHERE entry_id = %s AND key = %s",
-        [entry_id, score_key])
-    tournament_round_entry = cur.fetchone()
+
+    tournament_round_entry = db.session.\
+        query(Score, ScoreKey, ScoreCategory, TournamentEntry, RoundScore).\
+        join(ScoreKey).join(ScoreCategory).join(TournamentEntry).\
+        join(RoundScore).filter(and_(
+            TournamentEntry.id == entry_id, ScoreKey.key == score_key)).first()
 
     if tournament_round_entry is None:
         return None
 
-    cur.execute(
-        "SELECT g.id, g.table_num, g.protected_object_id \
-        FROM game g INNER JOIN game_entrant e ON g.id = e.game_id \
-        WHERE g.tourn = %s AND g.round_num = %s AND e.entrant_id = %s",
-        tournament_round_entry)
-    game_id_table = cur.fetchone()
+    tournament_round_entry = [
+        tournament_round_entry[2].tournament_id,
+        tournament_round_entry[4].round_id,
+        tournament_round_entry[3].id
+    ]
 
-    cur.execute(
-        "SELECT entrant_id FROM game_entrant WHERE game_id = %s",
-        [game_id_table[0]])
-    entrants = [Entry(entry_id=x[0]) for x in cur.fetchall()]
-    if len(entrants) == 1:
-        entrants.append('BYE')
+    game = TournamentGame.query.join(GameEntrant).filter(and_(
+        TournamentGame.tourn == tournament_round_entry[0],
+        TournamentGame.round_num == tournament_round_entry[1],
+        GameEntrant.entrant_id == tournament_round_entry[2])).first()
 
-    return Game(entrants,
-                game_id=game_id_table[0],
+    return Game(GameEntrant.query.filter_by(game_id=game.id).all(),
+                game_id=game.id,
                 tournament_id=tournament_round_entry[0],
                 round_id=tournament_round_entry[1],
-                table_number=game_id_table[1],
-                protected_object_id=tournament_round_entry[2])
+                table_number=game.table_num,
+                protected_object_id=game.protected_object_id)
 
 # pylint: disable=undefined-variable
 class Game(object):
@@ -60,8 +57,8 @@ class Game(object):
         self.tournament_id = tournament_id
         self.round_id = round_id
         self.table_number = table_number
-        self.entry_1 = None if entrants[0] == 'BYE' else entrants[0].entry_id
-        self.entry_2 = None if entrants[1] == 'BYE' else entrants[1].entry_id
+        self.entry_1 = entrants[0].entrant_id
+        self.entry_2 = None if len(entrants) == 1 else entrants[1].entrant_id
         self.protected_object_id = protected_object_id
 
     def num_entrants(self):
@@ -73,7 +70,6 @@ class Game(object):
         # pylint: disable=no-member
         return TournamentGame.query.filter_by(id=self.game_id).first()
 
-    @db_conn()
     def is_score_entered(self):
         """
         Checks if all scores for the game are entered and updates game row if
@@ -95,7 +91,6 @@ class Game(object):
         return None not in scores_entered \
         and len(scores_entered) == (scores_for_round * self.num_entrants())
 
-    @db_conn()
     def scores_entered(self):
         """
         Get a list of all scores entered for the game by all entrants.
