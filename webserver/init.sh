7604cf17c2013383049d4aa6ba786c0826685ca4
Robert Higgins <robert.h.higgins@gmail.com>
2016-03-22 11:34:47 +1300

    [Webserver] Minor change to install script

--- webserver/init.sh
+++ webserver/init.sh
@@ -1,11 +1,11 @@
-python manage.py syncdb --noinput
+#!/bin/bash
 
 python manage.py migrate
 
 if [ "$ROOT_PASSWORD" == "" ]; then
     # Root password environment variable is not set; so, load it from config.ini
-    echo "from ConfigParser import SafeConfigParser; parser = SafeConfigParser(); parser.read('/webapp/config.ini'); from django.contrib.auth.models import User; print 'Root user already exists' if User.objects.filter(username='root') else User.objects.create_superuser('higgins', 'robert.h.higgins@gmail.com.com', parser.get('general', 'ROOT_PASSWORD'))" | python manage.py shell
+    echo "from ConfigParser import SafeConfigParser; parser = SafeConfigParser(); parser.read('/webapp/config.ini'); from django.contrib.auth.models import User; print 'Root user already exists' if User.objects.filter(username='root') else User.objects.create_superuser('root', 'robert.h.higgins@gmail.com', parser.get('general', 'ROOT_PASSWORD'))" | python manage.py shell
 else
     # Root password environment variable IS set; so, use it
-    echo "import os; from django.contrib.auth.models import User; print 'Root user already exists' if User.objects.filter(username='root') else User.objects.create_superuser('higgins', 'robert.h.higgins@gmail.com.com', os.environ['ROOT_PASSWORD'])" | python manage.py shell
+    echo "import os; from django.contrib.auth.models import User; print 'Root user already exists' if User.objects.filter(username='root') else User.objects.create_superuser('root', 'robert.h.higgins@gmail.com', os.environ['ROOT_PASSWORD'])" | python manage.py shell
 fi
