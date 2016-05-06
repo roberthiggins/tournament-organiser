c30594671f4af4e37ed942b1ec53fc6776532201
Robert Higgins <robert.h.higgins@gmail.com>
2016-05-06 15:32:15 +1200

    [Unit_tests] Using tournamentinjector in registration

--- daoserver/src/unit_tests/registration.py
+++ daoserver/src/unit_tests/registration.py
@@ -6,53 +6,43 @@ from flask.ext.testing import TestCase
 
 from app import create_app
 from models.account import Account
-from models.registration import TournamentRegistration
-from models.tournament import db as tournament_db, Tournament as TournamentDAO
+from models.db_connection import db
+from models.registration import TournamentRegistration as TReg
 
-# pylint: disable=no-member,no-init,invalid-name,missing-docstring
+from unit_tests.tournament_injector import TournamentInjector
+
+# pylint: disable=no-member,invalid-name,missing-docstring
 class TournamentRegistrations(TestCase):
 
     def create_app(self):
-
         # pass in test configuration
         return create_app()
 
     def setUp(self):
-        tournament_db.create_all()
+        db.create_all()
+        self.injector = TournamentInjector()
+        self.applicant = 'test_register_applicant_1'
+        Account(self.applicant, 'spy@strahotski.com').write()
 
     def tearDown(self):
-        tournament_db.session.remove()
+        TReg.query.filter_by(player_id=self.applicant).delete()
+        Account.query.filter_by(username=self.applicant).delete()
+        self.injector.delete()
+        db.session.remove()
 
     def test_register(self):
         """Register a user for a tournament"""
-        tournament_1 = 'loving_strahotski'
-        tournament_2 = 'loving_casey'
-        tournament_3 = 'hating_chuck'
-        player_1 = 'strahotski'
-        tournament = TournamentDAO(tournament_3)
-        tournament.date = datetime.date.today() + datetime.timedelta(days=1)
-        tournament.write()
-        tournament = TournamentDAO(tournament_1)
-        tournament.date = datetime.date.today()
-        tournament.write()
-        tournament = TournamentDAO(tournament_2)
-        tournament.date = datetime.date.today()
-        tournament.write()
-        Account(player_1, 'spy@strahotski.com').write()
+        t_1 = 'test_register_1'
+        self.injector.inject(t_1, num_players=0)
+        t_2 = 'test_register_2'
+        self.injector.inject(t_2, num_players=0)
+        t_3 = 'test_register_3'
+        self.injector.inject(t_3, num_players=0, date=datetime.datetime.now())
 
-        # First rego good
-        TournamentRegistration(player_1, tournament_1).write()
-        # another day good
-        TournamentRegistration(player_1, tournament_3).write()
+        TReg(self.applicant, t_1).write()
+        TReg(self.applicant, t_2).write()
         # Repeat bad
-        self.assertRaises(
-            ValueError,
-            TournamentRegistration(player_1, tournament_1).write)
+        self.assertRaises(ValueError, TReg(self.applicant, t_1).write)
+        self.assertRaises(ValueError, TReg(self.applicant, t_2).write)
         # Same day bad
-        self.assertRaises(
-            ValueError,
-            TournamentRegistration(player_1, tournament_2).write)
-        # Repeat bad
-        self.assertRaises(
-            ValueError,
-            TournamentRegistration(player_1, tournament_3).write)
+        self.assertRaises(ValueError, TReg(self.applicant, t_3).write)
