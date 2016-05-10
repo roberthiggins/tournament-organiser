b4808cb1da4a2aeeabcb429898bac4a3a0d4a768
Robert Higgins <robert.h.higgins@gmail.com>
2016-05-10 13:45:07 +1200

    [Daoserver] Adding write_to_db helper function for db_connection

--- daoserver/src/models/db_connection.py
+++ daoserver/src/models/db_connection.py
@@ -10,3 +10,11 @@ from flask.ext.sqlalchemy import SQLAlchemy
 # pylint: disable=invalid-name
 db = SQLAlchemy()
 
+def write_to_db(dao):
+    """Write the DAO to the db"""
+    try:
+        db.session.add(dao)
+        db.session.commit()
+    except Exception:
+        db.session.rollback()
+        raise
