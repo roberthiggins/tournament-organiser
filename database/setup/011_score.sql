9b6c68b74352daf6977c556900ee6408865dbd6b
Robert Higgins <robert.h.higgins@gmail.com>
2016-05-18 14:14:39 +1200

    [Db] Tidy up of score table setup

--- database/setup/011_score.sql
+++ database/setup/011_score.sql
@@ -1,3 +1,20 @@
+/*
+A score_category is essentially a way to classify scores that might have to be
+modified from their raw values. e.g. A battle score might be worth 60% of the
+tournament points; A painting score might be ignored for determing matchups; etc.
+*/
+
+CREATE TABLE score_category(
+    id                  SERIAL PRIMARY KEY,
+    tournament_id       VARCHAR NOT NULL REFERENCES tournament(name),
+    display_name        VARCHAR NOT NULL,
+    percentage          INTEGER NOT NULL DEFAULT 100,
+    per_tournament      BOOLEAN NOT NULL DEFAULT FALSE,
+    min_val             INTEGER,
+    max_val             INTEGER
+);
+COMMENT ON COLUMN score_category.percentage IS 'The raw score should be multiplied by this.';
+
 CREATE TABLE score_key (
     id                  SERIAL UNIQUE,
     key                 VARCHAR NOT NULL,
