994537db68d2db9696fadc89ca6c58142032d8b3
Robert Higgins <robert.h.higgins@gmail.com>
2016-05-26 15:32:36 +1200

    [Daoserver] Removing write_to_db

--- daoserver/src/models/db_connection.py
+++ daoserver/src/models/db_connection.py
@@ -4,17 +4,7 @@ This sets up a connection to a database.
 Currently there is only one db so all connections can come through here.
 """
 
-
 from flask.ext.sqlalchemy import SQLAlchemy
 
 # pylint: disable=invalid-name
 db = SQLAlchemy()
-
-def write_to_db(dao):
-    """Write the DAO to the db"""
-    try:
-        db.session.add(dao)
-        db.session.commit()
-    except Exception:
-        db.session.rollback()
-        raise
