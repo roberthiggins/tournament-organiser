04c809afe31c7c189367efcf3fa836298fa04471
Robert Higgins <robert.h.higgins@gmail.com>
2016-05-30 15:46:49 +1200

    [Daosever] Moving config file injection to runtime

--- daoserver/src/app.py
+++ daoserver/src/app.py
@@ -3,7 +3,6 @@ Application for running the DAO API
 """
 
 import datetime
-import ConfigParser
 import os
 import jsonpickle
 
@@ -14,16 +13,13 @@ from permissions import set_up_permissions
 # pylint: disable=W0621
 def create_app():
     """Config for the app"""
-    config = ConfigParser.ConfigParser()
-    config.read('/webapp/environment_config.ini')
-
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = \
         'postgresql://docker:{}@{}:{}/{}'.format(
-            config.get('DATABASE', 'DB_PASSWORD'),
+            os.environ['DB_PASSWORD'],
             os.environ['DB_PORT_5432_TCP_ADDR'],
             os.environ['DB_PORT_5432_TCP_PORT'],
-            config.get('DATABASE', 'DB_NAME'))
+            os.environ['DB_NAME'])
 
     from models.db_connection import db as db_conn
     db_conn.init_app(app)
--- daoserver/src/db_connections/db_connection.py
+++ daoserver/src/db_connections/db_connection.py
@@ -4,7 +4,6 @@ Class to make a connection to the db
 
 from functools import wraps
 
-import ConfigParser
 import os
 import psycopg2
 
@@ -14,14 +13,12 @@ class DBConnection(object):
     """
     def __init__(self):
 
-        config = ConfigParser.ConfigParser()
-        config.read('/webapp/environment_config.ini')
         self.con = None
         self.config = {
             'db_host': os.environ['DB_PORT_5432_TCP_ADDR'],
-            'db_name': config.get('DATABASE', 'DB_NAME'),
+            'db_name': os.environ['DB_NAME'],
             'db_port': os.environ['DB_PORT_5432_TCP_PORT'],
-            'db_pass': config.get('DATABASE', 'DB_PASSWORD')
+            'db_pass': os.environ['DB_PASSWORD']
         }
         try:
             self.con = psycopg2.connect(
