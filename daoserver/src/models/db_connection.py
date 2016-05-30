91a741a2cdc7a2b4862ea9b3a54b0168055e9589
Robert Higgins <robert.h.higgins@gmail.com>
2016-05-30 17:39:20 +1200

    [Refactor][Daoserver] Replacing deprecated library

--- daoserver/src/models/db_connection.py
+++ daoserver/src/models/db_connection.py
@@ -4,7 +4,7 @@ This sets up a connection to a database.
 Currently there is only one db so all connections can come through here.
 """
 
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 # pylint: disable=invalid-name
 db = SQLAlchemy()
