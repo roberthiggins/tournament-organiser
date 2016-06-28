cb462170413153164232e74d1ead64c97a5389ba
Robert Higgins <robert.h.higgins@gmail.com>
2016-06-28 16:50:44 +1200

    [Daoserver] Adding API for getting list of tournament entries

--- daoserver/src/dao.py
+++ daoserver/src/dao.py
@@ -117,6 +117,21 @@ def main():
     return make_response('daoserver', 200)
 
 # Page actions
+@APP.route('/<tournament_id>/entries', methods=['GET'])
+def tournament_entries(tournament_id):
+    """
+    Return a list of the entrants for the tournament
+    """
+    # pylint: disable=no-member
+    if not Tournament(tournament_id).exists_in_db:
+        return make_response(
+            'Tournament {} doesn\'t exist'.format(tournament_id), 404)
+
+    entries = [ent.player_id for ent in \
+        TournamentEntry.query.filter_by(tournament_id=tournament_id).all()]
+    return Response(jsonpickle.encode(entries, unpicklable=False),
+                    mimetype='application/json')
+
 @APP.route('/listTournaments', methods=['GET'])
 def list_tournaments():
     """
