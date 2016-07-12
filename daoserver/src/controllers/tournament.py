69a50e2aa2e4eb751727370901e98feab6389f0b
Robert Higgins <robert.h.higgins@gmail.com>
2016-07-12 12:51:48 +1200

    [Refactor][Daoserver] Using pre-processor for tournament controller

--- daoserver/src/controllers/tournament.py
+++ daoserver/src/controllers/tournament.py
@@ -3,7 +3,7 @@ All tournament interactions.
 """
 import json
 import jsonpickle
-from flask import Blueprint, request
+from flask import Blueprint, g, request
 from sqlalchemy.exc import IntegrityError
 
 from controllers.request_helpers import enforce_request_variables, \
@@ -15,6 +15,14 @@ from models.dao.tournament_round import TournamentRound
 from models.tournament import Tournament, ScoreCategoryPair
 
 TOURNAMENT = Blueprint('TOURNAMENT', __name__)
+
+@TOURNAMENT.url_value_preprocessor
+# pylint: disable=unused-argument
+def get_tournament(endpoint, values):
+    """Retrieve tournament_id from URL and ensure the tournament exists"""
+    g.tournament_id = values.pop('tournament_id', None)
+    if g.tournament_id:
+        g.tournament = Tournament(g.tournament_id)
 
 @TOURNAMENT.route('', methods=['POST'])
 @text_response
@@ -37,20 +45,20 @@ def add_tournament():
         format(inputTournamentName, inputTournamentDate)
 
 @TOURNAMENT.route('/<tournament_id>/missions', methods=['GET'])
-def list_missions(tournament_id):
+def list_missions():
     """GET list of missions for a tournament."""
     return jsonpickle.encode(
-        [x.mission for x in Tournament(tournament_id).get_dao().rounds],
+        [x.mission for x in g.tournament.get_dao().rounds],
         unpicklable=False)
 
 @TOURNAMENT.route('/<tournament_id>/score_categories', methods=['GET'])
 @json_response
-def list_score_categories(tournament_id):
+def list_score_categories():
     """
     GET the score categories set for the tournament.
     e.g. [{ 'name': 'painting', 'percentage': 20, 'id': 2 }]
     """
-    return Tournament(tournament_id).list_score_categories()
+    return g.tournament.list_score_categories()
 
 @TOURNAMENT.route('/', methods=['GET'])
 @json_response
@@ -70,7 +78,7 @@ def list_tournaments():
 @TOURNAMENT.route('/<tournament_id>/register', methods=['POST'])
 @text_response
 @enforce_request_variables('inputUserName')
-def register(tournament_id):
+def register():
     # pylint: disable=undefined-variable
 
     """
@@ -78,7 +86,7 @@ def register(tournament_id):
     Expects:
         - inputUserName - Username of player applying
     """
-    rego = TournamentRegistration(inputUserName, tournament_id)
+    rego = TournamentRegistration(inputUserName, g.tournament_id)
     rego.clashes()
 
     try:
@@ -92,12 +100,11 @@ def register(tournament_id):
 @TOURNAMENT.route('/<tournament_id>/missions', methods=['POST'])
 @text_response
 @enforce_request_variables('missions')
-def set_missions(tournament_id):
+def set_missions():
     # pylint: disable=undefined-variable
 
     """POST to set the missions for a tournament.A list of strings expected"""
-    tourn = Tournament(tournament_id)
-    rounds = tourn.details()['rounds']
+    rounds = g.tournament.details()['rounds']
     try:
         json_missions = json.loads(missions)
     except TypeError:
@@ -105,10 +112,11 @@ def set_missions(tournament_id):
 
     if len(json_missions) != int(rounds):
         raise ValueError('Tournament {} has {} rounds. \
-            You submitted missions {}'.format(tournament_id, rounds, missions))
+            You submitted missions {}'.\
+            format(g.tournament_id, rounds, missions))
 
     for i, mission in enumerate(json_missions):
-        rnd = tourn.get_round(i + 1)
+        rnd = g.tournament.get_round(i + 1)
         # pylint: disable=no-member
         rnd.mission = mission if mission else \
             TournamentRound.__table__.c.mission.default.arg
@@ -120,14 +128,12 @@ def set_missions(tournament_id):
 @TOURNAMENT.route('/<tournament_id>/score_categories', methods=['POST'])
 @text_response
 @enforce_request_variables('categories')
-def set_score_categories(tournament_id):
+def set_score_categories():
     # pylint: disable=undefined-variable
 
     """
     POST to set tournament categories en masse
     """
-    tourn = Tournament(tournament_id)
-
     new_categories = []
     try:
         cats = json.loads(categories)
@@ -143,16 +149,16 @@ def set_score_categories(tournament_id):
         new_categories.append(
             ScoreCategoryPair(cat[0], cat[1], cat[2], cat[3], cat[4]))
 
-    tourn.set_score_categories(new_categories)
+    g.tournament.set_score_categories(new_categories)
 
     return 'Score categories set: {}'.\
         format(', '.join([str(cat.display_name) for cat in new_categories]))
 
 @TOURNAMENT.route('/<tournament_id>', methods=['GET'])
 @json_response
-def tournament_details(tournament_id=None):
+def tournament_details():
     """
     GET to get details about a tournament. This includes entrants and format
     information
     """
-    return Tournament(tournament_id).details()
+    return g.tournament.details()
