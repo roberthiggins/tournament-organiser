9663e967826f070993ddc6ee260eee25d1bca1fa
Robert Higgins <robert.h.higgins@gmail.com>
2016-08-15 15:01:22 +1200

    [Webserver] Re-direct entergamescore

--- webserver/src/public/views.py
+++ webserver/src/public/views.py
@@ -7,12 +7,7 @@ import json
 import urllib2
 
 from django.contrib import auth
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseNotFound, \
-HttpResponseRedirect
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-from public.forms import EnterGameScoreForm
+from django.http import HttpResponseRedirect
 from public.view_helpers import from_dao
 
 NODE_URL = 'http://{}:{}'.format(
@@ -40,58 +35,11 @@ def enter_score(request, tournament_id, username):      # pylint: disable=W0613
     return HttpResponseRedirect('{}/tournament/{}/entry/{}/enterscore'.\
         format(NODE_URL, tournament_id, username))
 
-def get_next_game_info(t_id, user):
-    """Get the next game for entry"""
-    try:
-        resp = from_dao('/tournament/{}/entry/{}/nextgame'.format(t_id, user))
-        next_game_info = json.loads(resp.content)
-        if not next_game_info:
-            raise ValueError
-
-        return next_game_info
-    except ValueError:
-        raise ValueError('No current game found! please contact TO.')
-
-@login_required
+# pylint: disable=unused-argument
 def enter_score_for_game(request, t_id, user):
     """ Enter a score for a single game"""
-    try:
-        entry_info(t_id, user)
-        next_game_info = get_next_game_info(t_id, user)
-    except ValueError as err:
-        return HttpResponseNotFound(err)
-
-    cats = [(x['name'], x['name']) for x in categories_info(t_id) \
-            if not x['per_tournament']]
-    form = EnterGameScoreForm(game_id=next_game_info['game_id'],
-                              poster=request.user.username,
-                              categories=cats)
-
-    if request.method == 'POST':
-        form = EnterGameScoreForm(data=request.POST,
-                                  game_id=next_game_info['game_id'],
-                                  poster=request.user.username,
-                                  categories=cats)
-
-        if form.is_valid():                     # pylint: disable=no-member
-            url = '/tournament/{}/entry/{}/entergamescore'.format(t_id, user)
-            response = from_dao(url, form, request)
-            if  response.status_code == 200:
-                return HttpResponse(response)
-            else:
-                form.add_error(None, response.content)  # pylint: disable=E1103
-
-    return render_to_response(
-        'enter-game-score.html',
-        {
-            'categories': cats,
-            'form': form,
-            'tournament': t_id,
-            'tournament_round': next_game_info['round'],
-            'username': user
-        },
-        RequestContext(request)
-    )
+    return HttpResponseRedirect('{}/tournament/{}/entry/{}/entergamescore'.\
+        format(NODE_URL, t_id, user))
 
 # pylint: disable=unused-argument
 def entry_list(request, tournament_id):
