0a7fb453f40bf8090451f9cbfe1a3775cc11ee54
Robert Higgins <robert.h.higgins@gmail.com>
2016-09-14 13:01:22 +1200

    [Refactor][Daoserver] Pre-processor for tournament_id in tournament_round

--- daoserver/src/controllers/tournament_round.py
+++ daoserver/src/controllers/tournament_round.py
@@ -1,7 +1,7 @@
 """
 Individual rounds in a tournament
 """
-from flask import Blueprint
+from flask import Blueprint, g
 
 from controllers.request_helpers import enforce_request_variables, \
 json_response, text_response
@@ -9,14 +9,24 @@ from models.tournament import Tournament
 
 TOURNAMENT_ROUND = Blueprint('TOURNAMENT_ROUND', __name__)
 
+@TOURNAMENT_ROUND.url_value_preprocessor
+# pylint: disable=unused-argument
+def get_tournament(endpoint, values):
+    """Retrieve tournament_id from URL and ensure the tournament exists"""
+    g.tournament_id = values.pop('tournament_id', None)
+    g.tournament = Tournament(g.tournament_id)
+    if not g.tournament.exists_in_db:
+        raise ValueError('Tournament {} not found in database'.\
+            format(g.tournament_id))
+
 @TOURNAMENT_ROUND.route('/<round_id>', methods=['GET'])
 @json_response
-def get_round_info(tournament_id, round_id):
+def get_round_info(round_id):
     """
     GET the information about a round
     """
     no_draw = AttributeError('No draw is available')
-    tourn = Tournament(tournament_id)
+    tourn = Tournament(g.tournament_id)
     rnd = tourn.get_round(round_id)
     try:
         draw = tourn.make_draw(round_id)
@@ -41,7 +51,7 @@ def get_round_info(tournament_id, round_id):
 @TOURNAMENT_ROUND.route('', methods=['POST'])
 @text_response
 @enforce_request_variables('numRounds')
-def set_rounds(tournament_id):
+def set_rounds():
     """Set the number of rounds for a tournament"""
 
     # pylint: disable=undefined-variable
@@ -52,5 +62,5 @@ def set_rounds(tournament_id):
     except ValueError:
         raise ValueError('Set at least 1 round')
 
-    Tournament(tournament_id).set_number_of_rounds(rounds)
+    Tournament(g.tournament_id).set_number_of_rounds(rounds)
     return 'Rounds set: {}'.format(rounds)
