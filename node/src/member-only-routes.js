98723a3f911475aa889d84010fd02e04861addb5
Robert Higgins <robert.h.higgins@gmail.com>
2016-09-23 11:46:24 +1200

    [Node] Getting user actions from daoserver

--- node/src/member-only-routes.js
+++ node/src/member-only-routes.js
@@ -1,11 +1,30 @@
 // Only members can see these routes
 
-var express  = require('express'),
-    feedback = require("./feedback"),
-    router   = express.Router(),
-    users    = require("./users");
+var DAOAmbassador = require("./dao-ambassador"),
+    express       = require('express'),
+    feedback      = require("./feedback"),
+    router        = express.Router(),
+    users         = require("./users");
 
 var injectAuthUser = [users.injectUserIntoRequest, users.ensureAuthenticated];
+
+router.route("/devindex")
+    .get(injectAuthUser, function(req, res) {
+        res.render("basic", {src_loc: "/devindex.js"});
+    });
+router.route("/devindex/content")
+    .get(injectAuthUser, function(req, res) {
+        var url = "/user/" + req.user.username + "/actions";
+
+        DAOAmbassador.getFromDAORequest(
+            req,
+            res,
+            url,
+            function(responseBody) {
+                var transformer = require("./models/devindex.js");
+                res.status(200).json(transformer.transform(responseBody));
+            });
+    });
 
 router.route("/feedback")
     .get(injectAuthUser, function(req, res) {
@@ -18,6 +37,5 @@ router.route("/feedback")
 
 router.route("/logout")
     .get(injectAuthUser, users.logout);
-
 
 module.exports = router;
--- node/src/public-routes.js
+++ node/src/public-routes.js
@@ -9,22 +9,6 @@ router.route("/")
     .get(function(req, res) {
         res.render("basic", {src_loc: "/index.js"});
     });
-router.route("/devindex")
-    .get(function(req, res) {
-        res.render("basic", {src_loc: "/devindex.js"});
-    });
-router.route("/devindex/content")
-    .get(function(req, res) {
-        var content = require("./models/devindex.js");
-        res.send([
-            content.enterT,
-            content.orgT,
-            content.playT,
-            content.viewT,
-            content.feedback
-        ]);
-    });
-
 
 // User mgmt
 router.route("/login")
