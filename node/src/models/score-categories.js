b181c7207aee1d91c4a195b82db7edacf76c1149
Robert Higgins <robert.h.higgins@gmail.com>
2016-10-28 11:41:06 +1300

    [Refactor] Moving category sanitisation to model

--- node/src/models/score-categories.js
+++ node/src/models/score-categories.js
@@ -10,4 +10,55 @@ var emptyCategory = function() {
         };
 };
 
+// Sanitise score-categories for POSTing
+var cleanCategories = function(cats) {
+    var cleanedCategories = (cats || [])
+        .map(function booleanConversion(cat) {
+            var result = {};
+            for (var key in cat) {
+                if (cat[key] === "false") {
+                    result[key] = false;
+                }
+                else if (cat[key] === 'true') {
+                    result[key] = true;
+                }
+                else {
+                    result[key] = cat[key];
+                }
+            }
+            return result;
+            })
+        .map(function stripUnusedFields(cat) {
+            var result = {},
+                proto = emptyCategory();
+
+            for (var key in proto) {
+                result[key] = cat[key];
+            }
+            return result;
+            })
+        .filter(function stripEmptyCats(cat) {
+            var empty = emptyCategory();
+            for (var key in empty) {
+                if (cat[key] !== empty[key]) {
+                    return true;
+                }
+            }
+            return false;
+            });
+
+    var reqFields = ["name", "percentage", "min_val", "max_val"];
+    cleanedCategories
+        .forEach(function checkRequired(cat) {
+            reqFields.forEach(function(key) {
+                if ((cat[key] || "") === "") {
+                    throw "Please fill in all fields";
+                }
+            });
+        });
+
+    return cleanedCategories;
+};
+
+exports.cleanCategories = cleanCategories;
 exports.emptyScoreCategory = emptyCategory;
--- node/src/tournament-routes.js
+++ node/src/tournament-routes.js
@@ -178,62 +178,18 @@ router.route("/tournament/:tournament/categories")
         users.ensureAuthenticated,
         function(req, res){
 
-            var cleanedCategories = (req.body.categories || [])
-                .map(function booleanConversion(cat) {
-                    var result = {};
-                    for (var key in cat) {
-                        if (cat[key] === "false") {
-                            result[key] = false;
-                        }
-                        else if (cat[key] === 'true') {
-                            result[key] = true;
-                        }
-                        else {
-                            result[key] = cat[key];
-                        }
-                    }
-                    return result;
-                    })
-                .map(function stripUnusedFields(cat) {
-                    var result = {},
-                        proto = Category.emptyScoreCategory();
-
-                    for (var key in proto) {
-                        result[key] = cat[key];
-                    }
-                    return result;
-                    })
-                .filter(function stripEmptyCats(cat) {
-                    var empty = Category.emptyScoreCategory();
-                    for (var key in empty) {
-                        if (cat[key] !== empty[key]) {
-                            return true;
-                        }
-                    }
-                    return false;
-                    });
-
             try {
-                var reqFields = ["name", "percentage", "min_val", "max_val"];
-                cleanedCategories
-                    .forEach(function checkRequired(cat) {
-                        reqFields.forEach(function(key) {
-                            if ((cat[key] || "") === "") {
-                                throw "Please fill in all fields";
-                            }
-                        });
-                    });
+                var cleanCats = Category.cleanCategories(req.body.categories);
+                DAOAmbassador.postToDAORequest(
+                    req,
+                    res,
+                    "/tournament/" + req.params.tournament,
+                    {score_categories: cleanCats});
             }
             catch (err) {
                 res.status(400).json({error: err});
                 return;
             }
-
-            DAOAmbassador.postToDAORequest(
-                req,
-                res,
-                "/tournament/" + req.params.tournament,
-                {score_categories: cleanedCategories});
         });
 router.route("/tournament/:tournament/categories/content")
     .get(function(req, res) {
