bf397cb0dfe416ddd549cfae27098188f65b0534
Robert Higgins <robert.h.higgins@gmail.com>
2016-10-31 16:12:04 +1300

    [Node] Adding menu to index

--- node/src/views/index.js
+++ node/src/views/index.js
@@ -1,5 +1,5 @@
 /* global React ReactDOM:true */
-var UserTaskList = React.createClass({
+var IntroPage = React.createClass({
     render: function() {
         return (
             <div>
@@ -14,24 +14,52 @@ var UserTaskList = React.createClass({
                     <strong>Check out the Masters Rankings over at <a href="http://rankings.thefieldsofblood.com/region=1&amp;game=5">Fields of Blood</a>
                     </strong>
                 </p>
-                <span>
-                    <a href="signup">Sign Up</a>
-                </span>
-                <span>
-                    <a href="login">Login</a>
-                </span>
-                <span>
-                    <a href="tournaments">See a list of tournaments</a>
-                </span>
-                <span>
-                    <a href="suggestimprovement">Suggest Improvement</a>
-                </span>
            </div>
         );
     }
 });
 
+
+var UserActionList = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <div className="menu_title">Account</div>
+                <ul>
+                    <li>
+                        <a className="menu_link" href="signup">Sign Up</a>
+                    </li>
+                    <li>
+                        <a className="menu_link" href="login">Login</a>
+                    </li>
+                    <li>
+                        <a className="menu_link"
+                           href="suggestimprovement">Suggest Improvement</a>
+                    </li>
+                </ul>
+                <div className="menu_title">Play</div>
+                <ul>
+                    <li>
+                        <a className="menu_link"
+                           href="tournaments">See a list of tournaments</a>
+                    </li>
+                </ul>
+                <div className="menu_title">Feedback</div>
+                <ul>
+                    <li>
+                        <a className="menu_link"
+                           href="suggestimprovement">Suggest Improvement</a>
+                    </li>
+                </ul>
+            </div>);
+    }
+});
+
 ReactDOM.render(
-    <UserTaskList />,
+    <IntroPage />,
     document.getElementById("content")
+);
+ReactDOM.render(
+    <UserActionList />,
+    document.getElementById("menu")
 );
