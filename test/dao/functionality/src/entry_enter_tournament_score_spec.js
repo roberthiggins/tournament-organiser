1c7f819ef6f798628e44a57f5f172c2e59625a89
Robert Higgins <robert.h.higgins@gmail.com>
2016-11-21 17:13:07 +1300

    [Refactor][Test] More binding

--- test/dao/functionality/src/entry_enter_tournament_score_spec.js
+++ test/dao/functionality/src/entry_enter_tournament_score_spec.js
@@ -4,7 +4,7 @@ var frisby = require("frisby"),
     p2 = tournament + "_p_2",
     API = process.env.API_ADDR + "tournament/enter_score_test/entry/";
 
-var postScore = function(api, player, msg, user, key, score, code, resp){
+var postScore = function(api, player, user, msg, key, score, code, resp){
     var scoreKey = key ? key : "enter_score_test_per_tourn_1";
     frisby.create("POST tournament score: " + msg)
         .post(api + player + "/entertournamentscore",
@@ -35,21 +35,23 @@ describe("Enter a tournament score for an entry", function () {
         .toss();
 
     var post = postScore.bind(this, API, p1);
-    post("Random user", "charlie_murphy", null, 5, 403, "Permission denied");
-    post("Different entry", p2, null, 5, 403, "Permission denied");
-    post("Superuser", "superuser", "enter_score_test_per_tourn_su", 5, 200,
+    post("charlie_murphy", "Random user", null, 5, 403, "Permission denied");
+    post(p2, "Different entry", null, 5, 403, "Permission denied");
+    post("superuser", "Superuser", "enter_score_test_per_tourn_su", 5, 200,
         "Score entered for " + p1 + ": 5");
-    post("TO", "enter_score_test_to", "enter_score_test_per_tourn_to", 5, 200,
+    post("enter_score_test_to", "TO", "enter_score_test_per_tourn_to", 5, 200,
         "Score entered for " + p1 + ": 5");
-    post("Player", p1, null, 5, 200, "Score entered for " + p1 + ": 5");
 
-    post("Player enters a score twice", p1, null, 4, 400,
+
+    post = postScore.bind(this, API, p1, p1);
+    post("Player", null, 5, 200, "Score entered for " + p1 + ": 5");
+
+    post("Player enters a score twice", null, 4, 400,
         "4 not entered. Score is already set");
 
-    post("Score too low", p1, null, 0, 400, "Invalid score: 0");
-    post("Score too high", p1, null, 16, 400, "Invalid score: 16");
-    post("Fake category", p1, "non_existent", 5, 400,
-        "Unknown category: non_existent");
-    post("Per game category", p1, "enter_score_test_per_game_1", 5, 400,
+    post("Score too low", null, 0, 400, "Invalid score: 0");
+    post("Score too high", null, 16, 400, "Invalid score: 16");
+    post("Fake category", "non_ex", 5, 400, "Unknown category: non_ex");
+    post("Per game category", "enter_score_test_per_game_1", 5, 400,
         "enter_score_test_per_game_1 should be entered per-tournament");
 });
