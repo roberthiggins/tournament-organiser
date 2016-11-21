7ededc7d7fb869721f714104e39a9ee24dcb7154
Robert Higgins <robert.h.higgins@gmail.com>
2016-11-21 16:59:01 +1300

    [Refactor] [Test] magic number removal

--- test/dao/functionality/src/entry_enter_tournament_score_spec.js
+++ test/dao/functionality/src/entry_enter_tournament_score_spec.js
@@ -24,7 +24,7 @@ describe("Enter a tournament score for an entry", function () {
     "use strict";
 
     frisby.create("No auth enters a score")
-        .post(API + "enter_score_test_p_1/entertournamentscore",
+        .post(API + p1 + "/entertournamentscore",
             {
                 key: "enter_score_test_1",
                 value: 5
@@ -35,26 +35,21 @@ describe("Enter a tournament score for an entry", function () {
         .toss();
 
     var post = postScore.bind(this, API, p1);
-    post("Non-playing user", "charlie_murphy", null, 5,
-        403, "Permission denied");
+    post("Random user", "charlie_murphy", null, 5, 403, "Permission denied");
     post("Different entry", p2, null, 5, 403, "Permission denied");
-    post("Superuser", "superuser",
-        "enter_score_test_per_tourn_su", 5, 200,
-        "Score entered for enter_score_test_p_1: 5");
-    post("TO", "enter_score_test_to",
-        "enter_score_test_per_tourn_to", 5, 200,
-        "Score entered for enter_score_test_p_1: 5");
-    post("Player", p1, null, 5, 200,
-        "Score entered for enter_score_test_p_1: 5");
+    post("Superuser", "superuser", "enter_score_test_per_tourn_su", 5, 200,
+        "Score entered for " + p1 + ": 5");
+    post("TO", "enter_score_test_to", "enter_score_test_per_tourn_to", 5, 200,
+        "Score entered for " + p1 + ": 5");
+    post("Player", p1, null, 5, 200, "Score entered for " + p1 + ": 5");
 
     post("Player enters a score twice", p1, null, 4, 400,
         "4 not entered. Score is already set");
 
     post("Score too low", p1, null, 0, 400, "Invalid score: 0");
     post("Score too high", p1, null, 16, 400, "Invalid score: 16");
-    post("Fake category", p1, "non_existent",
-        5, 400, "Unknown category: non_existent");
-    post("Per game category", p1, "enter_score_test_per_game_1",
-        5, 400, "enter_score_test_per_game_1 should be " +
-        "entered per-tournament");
+    post("Fake category", p1, "non_existent", 5, 400,
+        "Unknown category: non_existent");
+    post("Per game category", p1, "enter_score_test_per_game_1", 5, 400,
+        "enter_score_test_per_game_1 should be entered per-tournament");
 });
